279f27ee59f9aa95d41ddb5542f3517eace2726a
Dan McGlinn <danmcglinn@gmail.com>
2016-07-11 11:47:24 +0200

    Add run all script

---
+++ run_all.R
@@ -0,0 +1,9 @@
+
+# download datasets
+# need to add this script
+# http://www.esapubs.org/archive/ecol/E094/195/SCBI_initial_woody_stem_census_2012.csv
+# 
+
+
+# filter raw data
+Rscript('./scripts/spat_empir_data_filering')
